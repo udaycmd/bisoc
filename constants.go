909b4cd47b3c9e0161e8eb790405d92b51124c88
Uday Tiwari <udaytiwari720@gmail.com>
2026-02-22 14:31:53 +0530

    Add license headers

--- constants.go
+++ constants.go
@@ -1,3 +1,7 @@
+// Copyright 2026 Uday Tiwari. All rights reserved.
+// Use of this source code is governed by MIT
+// license that can be found in the LICENSE file.
+
 package bisoc
 
 import "errors"
